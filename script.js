b4747d7d591e7e175364c2cde226da8ba61aaf7e
ENAIKA <naikaesther5@gmail.com>
2020-02-15 07:33:43 +0300

    add akanMale()

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// var maleArray = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];//sunday has index 0
-
 var weekday = function () {
     var d = new Date();
     var day = document.getElementById("day").value;
@@ -10,7 +8,8 @@ var weekday = function () {
     d.setMonth(parseInt(month) - 1);
     d.setFullYear(parseInt(year));
     return d.getDay();
-i}
+    i
+}
 // function centuryFromYear(year) {
 //     var x = Math.floor(year/100) + 1;
 //     return x; 
@@ -19,12 +18,10 @@ i}
 
 function akanFemale() {
     var femaleArray = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //sunday-saturday# sunday has index 0
-    var gender = document.getElementById("rad1").value;
-    if (gender === "0") {
         if (weekday() === 0) {
             return "Your Akan name is " + femaleArray[0];
         }
-        else if (weekday() ===1) {
+        else if (weekday() === 1) {
             return "Your Akan name is " + femaleArray[1];
 
         } else if (weekday() === 2) {
@@ -42,33 +39,41 @@ function akanFemale() {
         else {
             return "Your Akan name is " + femaleArray[6];
         }
-        
+
+    }  
+
+
+function akanMale() {
+    var maleArray = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];//sunday has index 0
+    if (weekday() === 0) {
+        return "Your Akan name is " + maleArray[0];
     }
-    //else { return "male"; }
-    //     if (check() === 0) {
-    //         return "Your Akan name is "  + femaleArray[0];
-    //     }
-    //     else if (check() = 1) {
-    //         return "Your Akan name is "  + femaleArray[1];
+    else if (weekday() === 1) {
+        return "Your Akan name is " + maleArray[1];
 
-    //     } else if (check() === 2) {
-    //         return "Your Akan name is "  + femaleArray[2];
-    //     }
-    //     else if (check() === 3) {
-    //         return "Your Akan name is "  + femaleArray[3];
-    //     }
-    //     else if (check() === 4) {
-    //         return "Your Akan name is "  + femaleArray[4];
-    //     }
-    //     else if (check() === 5) {
-    //         return "Your Akan name is "  + femaleArray[5];
-    //     }
-    //     else {
-    //         return "Your Akan name is " + femaleArray[6];
-    //     }
-    // }
+    } else if (weekday() === 2) {
+        return "Your Akan name is " + maleArray[2];
+    }
+    else if (weekday() === 3) {
+        return "Your Akan name is " + maleArray[3];
+    }
+    else if (weekday() === 4) {
+        return "Your Akan name is " + maleArray[4];
+    }
+    else if (weekday() === 5) {
+        return "Your Akan name is " + maleArray[5];
+    }
+    else {
+        return "Your Akan name is " + maleArray[6];
+    }
 }
-
+//output function
 function output() {
-    document.getElementById("display").innerHTML = akanFemale();
+    if((document.getElementById("rad1").checked) && (weekday()===0||1||2||3||4||5||6)){
+        document.getElementById("display").innerHTML = akanFemale();
+    }
+    else if((document.getElementById("rad2").checked) && (weekday()===0||1||2||3||4||5||6)){
+        document.getElementById("display").innerHTML = akanMale();
+    }
+    
 }
