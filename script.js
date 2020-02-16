8217b59e47d54a3ebb10c97d7b0f7d0fb6a97bad
ENAIKA <naikaesther5@gmail.com>
2020-02-16 19:56:03 +0300

    add validation for leap year

--- script.js
+++ script.js
@@ -7,8 +7,23 @@ var weekday = function () {
     d.setDate(parseInt(day));
     d.setMonth(parseInt(month) - 1);
     d.setFullYear(parseInt(year));
-    return d.getDay();
-    i
+     if ((month == 4 || month == 6 || month == 9 || month == 2 || month == 11) && parseInt(day) > 30) {
+        document.getElementById("badinput").innerHTML="Invalid date";
+         return false;
+      } else if (month == 2 && parseInt(year) % 100 === 0 && parseInt(year) % 400 !== 0 && parseInt(day) > 28) {
+        document.getElementById("badinput").innerHTML="Invalid date";
+        return false;
+      }
+      else if (month == 2 && parseInt(year) % 4 !== 0 && parseInt(day) > 28) {
+        document.getElementById("badinput").innerHTML="Invalid date";
+         return false;
+      }
+      else if (month == 2 && parseInt(day) > 29) {
+        document.getElementById("badinput").innerHTML="Invalid date";
+         return false;
+      } else {
+        return d.getDay();
+      }  
 }
 // function centuryFromYear(year) {
 //     var x = Math.floor(year/100) + 1;
@@ -72,29 +87,18 @@ function output() {
     var day = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
-    if ((document.getElementById("rad1").checked) && (isNaN(day) || day < 1 || day > 32)) {
+    if ((document.getElementById("rad1").checked) && (isNaN(day) || day < 1 || day > 31)) {
         alert("Please correctly fill day field");
-        document.getElementById("day").reset();
         return false;
     }
     else if (isNaN(month) || month < 1 || month > 12 ||month===null ) {
         alert("Please correctly fill month field");
         return false;
     }
-    else if((document.getElementById("month")==2)&& (document.getElementById("day")>30)){
-        alert("Please ttcu invalid day for Feburuary");
-    }
     else if (isNaN(year) || year < 1582 || year > 2020) {
         alert("Invalid year");
-    }
-    else if (((document.getElementById("rad1").checked) || (document.getElementById("rad2").checked)) &&((month ===2) && day > 30)) {
-        alert("invalid day for Feburuary");
         // document.getElementById("akanForm").reset();
 
-    }
-    else if ((document.getElementById("rad2").checked)&&((month == 2) && day > 30)) {
-        alert("invalid day for Feburuary");
-        // document.getElementById("akanForm").reset();
     }
     else if (document.getElementById("rad1").checked) {
         document.getElementById("display").innerHTML = akanFemale();
